a4e95dfec8b626d59bb96a460ef00e177e6a7275
Anubha <anubhasahay0206@gmail.com>
2025-07-19 18:53:24 +0530

    added sringboot project

---
+++ stockmarketanalytics/src/test/java/com/learning/stockmarketanalytics/StockmarketanalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.stockmarketanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmarketanalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
